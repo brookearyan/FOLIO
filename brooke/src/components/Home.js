8dcec938fa43b7cba4ac77fbff24fb121926fc11
github username <github email address>
2018-02-02 17:42:37 -0500

    sizing of css home page

--- brooke/src/components/Home.js
+++ brooke/src/components/Home.js
@@ -4,10 +4,10 @@ import FIanimation from './img-addresses/FIanimation';
 
 export const Home = () =>
   <section className="home-container">
-    <article className="brooke">
-      <BrookePic />
-    </article>
     <article className="summary-article">
+      <article className="brooke">
+        <BrookePic />
+      </article>
       <p className="summary">
         A versatile full-stack web developer with a focus on back-end development, and a keen understanding of front-end development needs . I bring strong experience in managing large teams, data analytics and sales/financial forecasting. Proven track record of client management & services, strong communication skills, and a dedication to efficiency and logistics.
       </p>
