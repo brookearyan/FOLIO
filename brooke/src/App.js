1c2e2603b7927b1f109d47abd8baf3c3a8b1633e
github username <github email address>
2018-02-02 00:35:06 -0500

    cleaned up css, new classnames

--- brooke/src/App.js
+++ brooke/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import { Home } from './components/Home';
 import { TechProjects } from './components/TechProjects';
 import { Background } from './components/Background';
-import { Navbar } from './components/Navbar'
 import { Burger } from './components/Burger'
 import './App.css';
 
@@ -14,8 +13,8 @@ export class App extends Component {
         <div className="App">
           <header className="App-header">
             <Burger />
-            <NavLink className="navlink" to="/" exact>
-              <h1 className="brooke">brooke a. ryan</h1>
+            <NavLink to="/" exact>
+              <h1 className="brooke-logo">brooke a. ryan</h1>
             </NavLink>
           </header>
           <section className="main">
--- brooke/src/components/Burger.js
+++ brooke/src/components/Burger.js
@@ -3,22 +3,21 @@ import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import { BurgerBars } from './BurgerIcon'
 
 export class Burger extends React.Component {
-
   render() {
     return (
       <div class="dropdown">
-        <button class="dropbtn">
+        <button class="burger-button">
           <BurgerBars />
         </button>
-          <div class="dropdown-content">
+        <div class="dropdown-options">
           <NavLink to="/background" exact>
             background
           </NavLink>
           <NavLink to="/projects" exact>
             projects
           </NavLink>
-          </div>
         </div>
+      </div>
     );
   }
 }
